1a044d343b0e6f98665a23c257af0db2a2d5d129
Sonja Kesic <gdjica.sonja.kesic@incode.com>
2024-07-10 19:34:05 -0600

    Version 7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.incode.didi"
         minSdk = 21
         targetSdk = 34
-        versionCode = 6
-        versionName = "0.0.6"
+        versionCode = 7
+        versionName = "0.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
