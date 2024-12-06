6e32cb6c03b36f5ea0f39549ee36425e49aac22f
Bojan Radivojevic <bomberaya@gmail.com>
2024-12-06 21:22:06 +0100

    Fixed a typo in URL that was causing InstantApp integration to not work properly

--- app/src/main/java/com/incode/verify/demo/Constants.kt
+++ app/src/main/java/com/incode/verify/demo/Constants.kt
@@ -3,7 +3,7 @@ package com.incode.verify.demo
 object Constants {
 
     const val TAG = "IncodeVerifyDemo"
-    const val DEMO_URL = "https://demo.incode.id/?client_id=AcmeSample&redirect_url=acmesample://sucess&origin=native"
+    const val DEMO_URL = "https://demo.incode.id/?client_id=AcmeSample&redirect_url=acmesample://success&origin=native"
     const val INCODE_URL = "https://app.incode.com/?url="
     const val HELP_URL = "https://incode.com/contact-us"
     const val TERMS_URL = "https://incode.id/terms"
